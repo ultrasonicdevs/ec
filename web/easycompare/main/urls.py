8c12968b606cc6fdb49dc68f2c44c2c13366f769
Wayodeni <levgeneral@yandex.ru>
2021-10-20 19:45:41 +0300

    Applied remote changes Set views namespace in the 'main' templates according to docs

--- web/easycompare/main/urls.py
+++ web/easycompare/main/urls.py
@@ -2,12 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
+app_name = 'main'
 urlpatterns = [
     path('', views.sections_list),
-<<<<<<< HEAD
-    path('<str:section_name>', views.section, name='section_objects')
-=======
     path('<str:section_name>', views.section, name="section_detail")
->>>>>>> 708c9995f2f5d5081d7a8cb48e321a94a9e00d75
 ]
