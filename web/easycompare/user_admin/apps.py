f3de1d0d2b6af2f88fea34fbea0ae5575ca3232b
Ruslan Ivlev <91348490+zzZJagerZzz@users.noreply.github.com>
2022-03-05 16:29:10 +0300

    changed the addition of the navbar logo on the page + added user_admin app

---
+++ web/easycompare/user_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserAdminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'user_admin'
--- web/easycompare/user_side/views.py
+++ web/easycompare/user_side/views.py
@@ -5,10 +5,10 @@ from django.views import View
 class MainView(View):
     @staticmethod
     def get(request):
-        return render(request, 'user_side/main_page.html')
+        return render(request, 'user_side/main_page.html', {'title': 'EasyCompare'})
 
 
 class ProductCardView(View):
     @staticmethod
     def get(request):
-        return render(request, 'user_side/product_card.html')
+        return render(request, 'user_side/product_card.html', {'title': 'EasyCompare'})
