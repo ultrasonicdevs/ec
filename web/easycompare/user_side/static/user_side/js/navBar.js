73a2088331f4b13a2f816268cec16fe9455c3761
foxyw <91390313+FoxyGDFD@users.noreply.github.com>
2022-03-14 22:15:13 +0300

    remake navbar & section panel

--- web/easycompare/user_side/static/user_side/js/navBar.js
+++ web/easycompare/user_side/static/user_side/js/navBar.js
@@ -1,108 +1,97 @@
-const searchBtn = document.querySelector('.search-btn');
+const searchBtn = document.querySelector('#display-form'),
+    searchPanel = document.querySelector('.search-panel'),
+    sectionsListHTML = document.getElementById('section-list'),
+    productTypeListHTML = document.getElementById('product-type-list');
 searchBtn.addEventListener('click', searchPanelDisplay);
 
+
 async function searchPanelDisplay() {
-    const searchPanel = document.querySelector('.search-panel'),
-        content = document.querySelector('.content'),
-        searchBtnImg = document.querySelector('#search-img'),
 
-        form = document.createElement('form'),
-        sectionsDiv = document.createElement('div'),
-        productTypesDiv = document.createElement('div'),
-        selectTitleS = document.createElement('h2'),
-        selectTitleP = document.createElement('h2'),
-        selectSection = document.createElement('select'),
-        selectProductType = document.createElement('select'),
-        optionS = document.createElement('option'),
-        optionP = document.createElement('option'),
-        submit = document.createElement('button');
+    const checkForm = document.getElementById('form-btn');
+    if (document.getElementById('form-btn').checked === false) {
+        const sectionList = await getJSON('http://127.0.0.1:8000/ec-admin/add-section/get-parent-sections/', "GET", null);
 
-    form.className ='form';
-    sectionsDiv.className = 'sections';
-    productTypesDiv.className = 'categories';
-    selectTitleS.className = 'search-block-header';
-    selectTitleS.appendChild(document.createTextNode('Выберите раздел'));
-    selectTitleP.className = 'search-block-header';
-    selectTitleP.appendChild(document.createTextNode('Выберите категорию'));
-    selectSection.id = 'select-section';
-    selectProductType.id = 'select-category';
-    selectProductType.disabled = true;
-    submit.className = 'submit';
-    optionS.appendChild(document.createTextNode('Выберите раздел'));
-    optionS.hidden = true;
-    optionS.selected = true;
-    optionP.appendChild(document.createTextNode('Выберите категорию'));
-    optionP.hidden = true;
-    optionP.selected = true;
-    submit.appendChild(document.createTextNode('Подтвердить'));
-
-    form.appendChild(sectionsDiv);
-    form.appendChild(productTypesDiv);
-    sectionsDiv.appendChild(selectTitleS);
-    sectionsDiv.appendChild(selectSection);
-    selectSection.appendChild(optionS);
-    productTypesDiv.appendChild(selectTitleP);
-    productTypesDiv.appendChild(selectProductType);
-    selectProductType.appendChild(optionP);
-    form.appendChild(submit);
-
-    if (searchPanel.style.display === 'none') {
-        searchPanel.appendChild(form);
-        searchPanel.style.opacity = '0';
-        setTimeout(() => {
-            content.style.filter = 'blur(1rem)';
-            searchPanel.style.display = 'flex';
+        for (let section of sectionList.sections) {
+            let sectionHTML = document.createElement('li');
+            let label = document.createElement('label'),
+                input = document.createElement('input'),
+                span = document.createElement('span');
+            input.type = 'radio';
+            input.id = section._id + '-input';
+            input.className =
+            label.htmlFor = input.id;
+            sectionHTML.className = 'section';
+            sectionHTML.id = section._id;
+            sectionHTML.appendChild(input);
+            sectionHTML.appendChild(label);
+            label.appendChild(span);
+            span.appendChild(document.createTextNode(section.name));
+            span.id = section._id + 'span';
+            span.className = 'span';
+            sectionsListHTML.appendChild(sectionHTML);
+        }
+        setTimeout(()=> {
+            searchPanel.style.display = 'block';
             searchPanel.style.opacity = '1';
-            searchBtnImg.src = 'static/user_side/img/cancel_menuBtn.svg';
+        },1);
+
+        const sections = document.getElementsByClassName('section');
+        [...sections].forEach(section => {
+            section.addEventListener('click', displayProductTypes);
         });
+            sectionsListHTML.style.flexWrap = 'wrap';
+            sectionsListHTML.style.height = '24rem';
+            sectionsListHTML.style.flexBasis = '70%';
     }
     else {
         searchPanel.style.opacity = '0';
         setTimeout(() => {
             searchPanel.style.display = 'none';
-            content.style.filter = 'none';
-            searchBtnImg.src = 'static/user_side/img/search_menuBtn.svg';
-        });
-        const delForm = document.querySelector('form');
-        delForm.remove()
-    }
-
-    const sectionListJSON = await getJSON('http://127.0.0.1:8000/ec-admin/add-section/get-parent-sections/', "GET", null),
-        sectionList = sectionListJSON.sections;
-    for (let section of sectionList) {
-        const option = document.createElement('option');
-        option.id = section._id;
-        option.appendChild(document.createTextNode(section.name))
-        selectSection.appendChild(option);
-    }
-
-    selectSection.addEventListener('change', selectProductTypeDisplay);
-
-    async function selectProductTypeDisplay() {
-        const index = selectSection.options.selectedIndex,
-            id = selectSection.options[index].id,
-            productTypesJSON = await getJSON(`http://127.0.0.1:8000/api/sections/${id}/product-types/`, "GET", null),
-            productTypes = productTypesJSON.product_types;
-
-        selectProductType.disabled = false;
-        selectProductType.innerHTML = '';
-
-        selectProductType.appendChild(optionP);
-        optionP.hidden = true;
-        optionP.selected = true;
-        for (let productType of productTypes) {
-            const option = document.createElement('option');
-            option.id = productType._id;
-            option.appendChild(document.createTextNode(productType.name))
-            selectProductType.appendChild(option);
-        }
+        },1);
+        sectionsListHTML.innerHTML = '';
+        productTypeListHTML.innerHTML = '';
     }
 }
 
-async function getJSON(url, method, body) {
-    let response = await fetch(url, {method, body});
-    response = await response.json();
-    console.log(response);
 
+async function displayProductTypes(event) {
+    productTypeListHTML.innerHTML = '';
+    let inputID = event.target.id,
+        liID = inputID.replace(/-input/,'');
+    console.log(liID);
+
+    sectionsListHTML.style.flexWrap = 'nowrap';
+    sectionsListHTML.style.height = '100%';
+    sectionsListHTML.style.flexBasis = '30%';
+
+    const spans = document.getElementsByClassName('span');
+    for(let span of spans) {
+        span.className = 'span grey';
+    }
+
+    const span = document.getElementById(liID + 'span');
+    span.className = 'span act';
+
+    const productTypesList = await getJSON(`http://127.0.0.1:8000/api/sections/${liID}/product-types/`, "GET", null);
+
+    for (let productType of productTypesList.product_types) {
+        let productTypeHTML = document.createElement('li');
+        productTypeHTML.id = productType._id;
+        let link = document.createElement('a');
+        // link.href = '#'
+        link.appendChild(document.createTextNode(productType.name));
+        productTypeHTML.appendChild(link);
+        productTypeListHTML.appendChild(productTypeHTML);
+    }
+}
+
+
+async function getJSON(url, method, body) {
+    let response = await fetch(url, {method, body,
+        headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       },}).then(res => res.json());
+    console.log(response);
     return response
 }
