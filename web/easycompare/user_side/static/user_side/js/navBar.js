bd82eee3a99eebaa20851212a3a8557f0d2dd40c
foxyw <91390313+FoxyGDFD@users.noreply.github.com>
2022-03-15 14:32:03 +0300

    less bug nav bar

--- web/easycompare/user_side/static/user_side/js/navBar.js
+++ web/easycompare/user_side/static/user_side/js/navBar.js
@@ -1,33 +1,19 @@
 const searchBtn = document.querySelector('#display-form'),
     searchPanel = document.querySelector('.search-panel'),
     sectionsListHTML = document.getElementById('section-list'),
-    productTypeListHTML = document.getElementById('product-type-list');
+    ulProductTypes = document.getElementById('product-type-list');
 searchBtn.addEventListener('click', searchPanelDisplay);
 
 
 async function searchPanelDisplay() {
     if (document.getElementById('form-btn').checked === false) {
         const sectionList = await getJSON('http://127.0.0.1:8000/ec-admin/add-section/get-parent-sections/', "GET", null);
-
         for (let section of sectionList.sections) {
-            let sectionHTML = document.createElement('li');
-            let label = document.createElement('label'),
-                input = document.createElement('input'),
-                span = document.createElement('span');
-
-            sectionHTML.className = 'section';
-            sectionHTML.id = section._id;
-            label.htmlFor = input.id;
-            input.type = 'radio';
-            input.id = section._id + '-input';
-            // input.className = 'input';
-            span.id = section._id + '-span';
-            span.className = 'span';
-            span.appendChild(document.createTextNode(section.name));
-            label.appendChild(span);
-            sectionHTML.appendChild(input);
-            sectionHTML.appendChild(label);
-            sectionsListHTML.appendChild(sectionHTML);
+            let liSection = document.createElement('li');
+            liSection.className = 'section';
+            liSection.id = section._id;
+            liSection.appendChild(document.createTextNode(section.name));
+            sectionsListHTML.appendChild(liSection);
         }
         setTimeout(()=> {
             searchPanel.style.display = 'block';
@@ -40,6 +26,7 @@ async function searchPanelDisplay() {
             sectionsListHTML.style.flexWrap = 'wrap';
             sectionsListHTML.style.height = '24rem';
             sectionsListHTML.style.flexBasis = '70%';
+
     }
     else {
         searchPanel.style.opacity = '0';
@@ -47,48 +34,56 @@ async function searchPanelDisplay() {
             searchPanel.style.display = 'none';
         });
         sectionsListHTML.innerHTML = '';
-        productTypeListHTML.innerHTML = '';
+        ulProductTypes.innerHTML = '';
     }
 }
 
 
 async function displayProductTypes(event) {
-    productTypeListHTML.innerHTML = '';
-    let inputID = event.target.id,
-        liID = inputID.replace(/-input/,'');
+    ulProductTypes.innerHTML = '';
+    ulProductTypes.style.display = 'none';
+    ulProductTypes.style.opacity = '0';
+    let liID = event.target.id;
 
     sectionsListHTML.style.flexWrap = 'nowrap';
     sectionsListHTML.style.height = '100%';
     sectionsListHTML.style.flexBasis = '30%';
 
-    // const spans = document.getElementsByClassName('span');
-    [...document.getElementsByClassName('span')].forEach(span => {
-        //span.className = 'span grey';
-    })
+    [...document.getElementsByClassName('section')].forEach(section => {
+        if (section.className === 'section act') {
+            section.className = 'section grey';
+        }
+        else if (section.className === 'section') {
+            section.className += ' grey';
+        }
+    });
 
-    // const span = document.getElementById(liID + '-span');
-    // span.className = 'span act';
+    const sectionAct = document.getElementById(liID);
+    sectionAct.className = 'section act';
 
     const productTypesList = await getJSON(`http://127.0.0.1:8000/api/sections/${liID}/product-types/`, "GET", null);
 
     for (let productType of productTypesList.product_types) {
-        let productTypeHTML = document.createElement('li');
-        productTypeHTML.id = productType._id;
+        let liProductType = document.createElement('li');
+        liProductType.id = productType._id;
         let link = document.createElement('a');
-        // link.href = '#'
         link.appendChild(document.createTextNode(productType.name));
-        productTypeHTML.appendChild(link);
-        productTypeListHTML.appendChild(productTypeHTML);
+        liProductType.appendChild(link);
+        ulProductTypes.appendChild(liProductType);
     }
+    ulProductTypes.style.display = 'block';
+    setTimeout(()=>{
+
+        ulProductTypes.style.opacity = '1';
+    }, 100);
+
 }
 
 
 async function getJSON(url, method, body) {
-    let response = await fetch(url, {method, body,
+    return await fetch(url, {method, body,
         headers : {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
-       },}).then(res => res.json())//.then(res => console.log(res));
-     console.log(response);
-     return response
+       },}).then(res => res.json())
 }
