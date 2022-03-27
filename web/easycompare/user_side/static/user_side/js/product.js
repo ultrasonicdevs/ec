b8782259288df2927e911361305b636b527778c9
foxyw <91390313+FoxyGDFD@users.noreply.github.com>
2022-03-27 18:46:12 +0300

    Price filter (not ready)

--- web/easycompare/user_side/static/user_side/js/product.js
+++ web/easycompare/user_side/static/user_side/js/product.js
@@ -1,11 +1,64 @@
-class productCard extends Block{
-    async displayCards() {
-        const typeID = document.URL.replace(`${location.protocol}//${location.host}/`, ''),
-            typeInfo = await new Block().getJSON(`${location.protocol}//${location.host}/api/product-types/${typeID}`, "GET", null),
-            productsJSON = await new Block().getJSON(`${location.protocol}//${location.host}/api/product-types/${typeID}products/`, "GET", null),
-            productsContainer = document.querySelector('#products');
-        document.title = `${typeInfo.name} | Easy Compare`;
+class Filter extends Block {
+    checkPrice(inputs, productJSON) {
+        let productsArray = [];
+        productJSON.forEach(product =>{
+            product.attributes.forEach(attribute => {
+                if (attribute.verbose_name === 'price') {
+                    if (Number(attribute.value) > inputs[0] && Number(attribute.value) < inputs[1]){
+                        productsArray.push(product);
+                    }
+                }
+            });
+        });
+        new Filter().displayCards({'products': productsArray});
+    }
 
+
+    rangeSliderInit(input, productsJSON) {
+        let startValue = Math.min.apply(null, input),
+            endValue = Math.max.apply(null, input);
+
+        const range = document.getElementById('slider-round'),
+            inputMin = document.getElementById('min'),
+            inputMax = document.getElementById('max');
+        const inputs = [inputMin, inputMax];
+        noUiSlider.create(range, {
+            start: [startValue, endValue],
+            connect: true,
+            range: {
+                'min': startValue,
+                'max': endValue
+            },
+            step: 1,
+        });
+        range.noUiSlider.on('update', function (values, handle) {
+            inputs[handle].value = parseInt(values[handle]);
+        });
+        inputMin.addEventListener('change', function () {
+            range.noUiSlider.set([this.value, null]);
+            new Filter().checkPrice(productsJSON);
+        });
+        inputMax.addEventListener('change', function () {
+            range.noUiSlider.set([null, this.value]);
+            new Filter().checkPrice(productsJSON);
+        });
+    }
+
+
+    async getProducts() {
+        const typeID = document.URL.replace(`${location.protocol}//${location.host}/`, ''),
+            typeInfo = await new Filter().getJSON(`${location.protocol}//${location.host}/api/product-types/${typeID}`, "GET", null),
+            productsJSON = await new Filter().getJSON(`${location.protocol}//${location.host}/api/product-types/${typeID}products/`, "GET", null);
+        document.title = `${typeInfo.name} | Easy Compare`;
+        console.log(productsJSON);
+        new Filter().displayCards(productsJSON);
+    }
+
+
+    displayCards(productsJSON) {
+        let priceList = [];
+        const productsContainer = document.querySelector('#products');
+        productsContainer.innerHTML = ''
         productsJSON.products.forEach(product => {
             console.log(product.preview_url)
             const link = document.createElement('a'),
@@ -34,9 +87,20 @@ class productCard extends Block{
             description.appendChild(price);
             link.appendChild(cardContainer)
             productsContainer.appendChild(link);
+
+            product.attributes.forEach(attribute => {
+                if (attribute.verbose_name === 'price') {
+                    price.appendChild(document.createTextNode(attribute.value));
+                    priceList.push(Number(attribute.value));
+                }
+                if (attribute.verbose_name === 'manufacturer') {
+                    manufacturer.appendChild(document.createTextNode(attribute.value));
+                }
+            });
         });
+        console.log(priceList);
+        new Filter().rangeSliderInit(priceList, productsJSON);
     }
 }
 
-
-window.addEventListener('load', new productCard().displayCards)
+window.addEventListener('load', new Filter().getProducts);
--- web/easycompare/user_side/static/user_side/js/rangeControl.js
+++ web/easycompare/user_side/static/user_side/js/rangeControl.js
@@ -1,27 +1,27 @@
-const rangeSliderInit = () => {
-  const range = document.getElementById('slider-round'),
-      inputMin = document.getElementById('min'),
-      inputMax = document.getElementById('max');
-  const inputs = [inputMin, inputMax];
-  noUiSlider.create(range, {
-      start: [0, 100],
-      connect: true,
-      range: {
-        'min': 0,
-        'max': 100
-      },
-      step: 1,
-    });
-  range.noUiSlider.on('update', function (values, handle) {
-    inputs[handle].value = parseInt(values[handle]);
-  });
-  inputMin.addEventListener('change', function () {
-    range.noUiSlider.set([this.value, null]);
-  });
-  inputMax.addEventListener('change', function () {
-    range.noUiSlider.set([null, this.value]);
-  });
-}
-
-
-window.addEventListener('DOMContentLoaded', rangeSliderInit)
+// const rangeSliderInit = () => {
+//   const range = document.getElementById('slider-round'),
+//       inputMin = document.getElementById('min'),
+//       inputMax = document.getElementById('max');
+//   const inputs = [inputMin, inputMax];
+//   noUiSlider.create(range, {
+//       start: [0, 100],
+//       connect: true,
+//       range: {
+//         'min': 0,
+//         'max': 100
+//       },
+//       step: 1,
+//     });
+//   range.noUiSlider.on('update', function (values, handle) {
+//     inputs[handle].value = parseInt(values[handle]);
+//   });
+//   inputMin.addEventListener('change', function () {
+//     range.noUiSlider.set([this.value, null]);
+//   });
+//   inputMax.addEventListener('change', function () {
+//     range.noUiSlider.set([null, this.value]);
+//   });
+// }
+//
+//
+// window.addEventListener('DOMContentLoaded', rangeSliderInit)
