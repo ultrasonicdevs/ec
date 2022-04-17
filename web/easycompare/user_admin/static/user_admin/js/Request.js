ffb432e28ef4550fbc95dd2abe7f0e2cd1b3db2d
Ruslan Ivlev <91348490+zzZJagerZzz@users.noreply.github.com>
2022-04-17 15:38:07 +0300

    refactor: Changed sendRequest method

--- web/easycompare/user_admin/static/user_admin/js/Request.js
+++ web/easycompare/user_admin/static/user_admin/js/Request.js
@@ -4,18 +4,29 @@ class Request {
     this.urlToProductTypes = `${location.protocol}//${location.host}/api/product-types/`;
   }
 
-  async sendRequest (url, method, body = null, hasCSRFtoken = null) {
-    const XCSRF = hasCSRFtoken ? this.getCookieByName('csrftoken') : null;
-    
+  async sendRequest (url, method, body = null, headersHTTP = null, hasCSRFtoken = null) {
+    const XCSRFtoken = hasCSRFtoken ? this.getCookieByName('csrftoken') : null;
+
+    const headers = {
+      'X-CSRFToken': XCSRFtoken,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    };
+
+    if (headersHTTP) {
+      for (let [key, value] of Object.entries(headersHTTP)) {
+        if (value instanceof Object) {
+          value = encodeURIComponent(JSON.stringify(value))
+        }
+        headers[key] = value;
+      }
+    }
+
     return await fetch(url, {
       method,
       body,
-      headers: {
-        'X-CSRFToken': XCSRF,
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-    }).then(data => data.json());
+      headers,
+    }).then(response => response.json())
   }
 
   getCookieByName(name) {
